8532a32592ff2743cbb414fe0fb02c6575a5a37d
Lokeshkumar <lokeshkumarn@ymail.com>
2019-03-30 13:08:26 +0530

    Update model.R
    Removed K_repeat and used Repeat Vector

--- analysis/model.R
+++ analysis/model.R
@@ -3,7 +3,8 @@ dt_model <- function() {
   company_code_input <- layer_input(shape = 1, name = "company_input")
   company_code_embedding <- company_code_input %>%
     layer_embedding(200, 49, name = "company_code_embedding") %>%
-    layer_flatten()
+    layer_flatten()%>%
+    layer_repeat_vector(9)
 
   encoded <- ay_seq_input %>%
     layer_masking(-99) %>%
@@ -12,7 +13,8 @@ dt_model <- function() {
   decoded <- encoded %>%
     layer_repeat_vector(9) %>%
     layer_gru(128, return_sequences = TRUE, dropout = 0.2, recurrent_dropout = 0.2) %>%
-    layer_lambda(f = function(x) layer_concatenate(list(x, k_repeat(company_code_embedding, 9))))
+    layer_lambda(f = function(x) layer_concatenate(list(x, company_code_embedding)))  
+    #layer_lambda(f = function(x) layer_concatenate(list(x, k_repeat(company_code_embedding, 9))))
 
   case_reserves_output <- decoded %>%
     time_distributed(layer_dense(units = 64, activation = "relu")) %>%
